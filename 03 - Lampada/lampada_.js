7cb49a5467fa0ae136213f80912ac27c00c595c4
Pierri Alexander Vidmar <65265714+pierrialexander@users.noreply.github.com>
2021-08-10 16:05:39 -0300

    Conclusão Projeto
    Conclusão projeto

--- 03 - Lampada/lampada_.js
+++ 03 - Lampada/lampada_.js
@@ -2,18 +2,18 @@ const turnOnOff = document.getElementById('turnOnOff')
 const lamp = document.getElementById('lamp')
 
 function isLampBroken() {
-  return lamp.src.indexOf ('quebrada') > -1
+  return lamp.src.indexOf('quebrada') > -1
 }
 
 function lampOn() {
-  if (!isLampBroken ()) {
+  if (!isLampBroken()) {
     lamp.src = './img/ligada.jpg'
     
   }
 }
 
 function lampOff() {
-  if (!isLampBroken ()) {
+  if (!isLampBroken()) {
     lamp.src = './img/desligada.jpg'
     turnOnOff.textContent = 'Ligar'
   }
@@ -33,8 +33,10 @@ function lampOnOff() {
   }
 }
 
+
+
 turnOnOff.addEventListener('click', lampOnOff)
 
-lamp.addEventListener('dblclick', lampBroken)
 lamp.addEventListener('mouseover', lampOn)
 lamp.addEventListener('mouseleave', lampOff)
+lamp.addEventListener('dblclick', lampBroken)
