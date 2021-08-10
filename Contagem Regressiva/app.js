c9ecd33c25d241a567a9b170715a95b5001e142e
Pierri Alexander Vidmar <65265714+pierrialexander@users.noreply.github.com>
2021-08-09 21:57:16 -0300

    Projeto concluido
    Projeto concluido

--- Contagem Regressiva/app.js
+++ Contagem Regressiva/app.js
@@ -1,15 +1,27 @@
+const secondsContainer = document.querySelector('#seconds')
+const minutesContainer = document.querySelector('#minutes')
+const hoursContainer = document.querySelector('#hours')
+const daysContainer = document.querySelector('#days')
+
 const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
 const updateCountdown = () => {
-  const currentTime = new Date() //Atual
-  const difference = newYearTime - currentTime //calcula os milisegundos que falta para ano novo
-  const days = Math.floor(difference / 1000 / 60 / 60 / 24) //arredonda em segundos para o ano novo
-  const hours = Math.floor(difference / 1000 / 60 / 60) % 24 //arredonda em segundos para o ano novo
-  const minutes = Math.floor(difference / 1000 / 60) % 60//arredonda em segundos para o ano novo
-  const seconds = Math.floor(difference / 1000) % 60 //arredonda em segundos para o ano novo
+const currentTime = new Date() //Atual
+const difference = newYearTime - currentTime //calcula os milisegundos que falta para ano novo
+const days = Math.floor(difference / 1000 / 60 / 60 / 24) //arredonda em segundos para o ano novo
+const hours = Math.floor(difference / 1000 / 60 / 60) % 24 //arredonda em segundos para o ano novo
+const minutes = Math.floor(difference / 1000 / 60) % 60//arredonda em segundos para o ano novo
+const seconds = Math.floor(difference / 1000) % 60 //arredonda em segundos para o ano novo
 
-  console.log(`Faltam ${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos, para o ano acabar!`)
+secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
+hoursContainer.textContent = hours < 10 ? '0' + hours : hours
+daysContainer.textContent = days < 10 ? '0' + days : days
+
+
 }
 
-console.log(updateCountdown())
+// metodo que invoca repetidamente uma função 
+setInterval(updateCountdown, 1000);
+
